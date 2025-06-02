9ff3905906851cbb80053bb7bc3afb7c57ae209a
tanvirmulla116660 <tanvir.mulla116660@marwadiuniversity.ac.in>
2025-06-02 17:22:29 +0530

    1st Commit

---
+++ LB01_first_program_01.py
@@ -0,0 +1,7 @@
+# Simple Python program to greet the user
+
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Print a greeting message
+print(f"Hello, {name}! Nice to meet you.")
